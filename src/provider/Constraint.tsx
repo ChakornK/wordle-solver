585bb9c9b847ce5da2be6b9f423efc79e8fbbc76
ChakornK <chakornk2007@gmail.com>
2025-12-26 15:53:17 -0800

    fix: update constraint state provider

--- src/provider/Constraint.tsx
+++ src/provider/Constraint.tsx
@@ -1,17 +1,22 @@
 import { type ComponentChildren, createContext } from "preact";
 import type { Constraints } from "../lib/solver";
+import { useState } from "preact/hooks";
 
-const Constraint = createContext<Constraints>(null);
+const Constraint = createContext(null);
 
 export const ConstraintProvider = ({
   children,
 }: {
   children: ComponentChildren;
 }) => {
+  const [constraints, setConstraints] = useState<Constraints>({
+    absent: [],
+    present: [],
+    incorrect: {},
+    correct: {},
+  });
   return (
-    <Constraint.Provider
-      value={{ absent: [], present: [], incorrect: {}, correct: {} }}
-    >
+    <Constraint.Provider value={{ constraints, setConstraints }}>
       {children}
     </Constraint.Provider>
   );
