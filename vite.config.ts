ad75a51c1fee674c400e9bc031eb3b5601414450
ChakornK <chakornk2007@gmail.com>
2025-12-26 19:15:39 -0800

    feat: add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -15,27 +15,33 @@ export default defineConfig({
         previewMiddlewareFallback: "/404",
       },
     }),
-
     {
       name: "meta-tags",
       transformIndexHtml(html) {
         return ((
           html: string,
-          { title, description }: { title: string; description: string }
+          {
+            title,
+            description,
+            image,
+          }: { title: string; description: string; image: string }
         ) => {
           const metaHtml = `
-		        <title>${title}</title>
-		        <meta property="og:description" content="${description}">
-		        <meta property="og:title" content="${title}">
-		        <meta name="description" content="${description}">
-		        <meta name="twitter:title" content="${title}">
-		        <meta name="twitter:description" content="${description}">
-		        <meta name="twitter:card" content="summary">`;
+            <title>${title}</title>
+            <meta property="og:description" content="${description}">
+            <meta property="og:title" content="${title}">
+            <meta property="og:image" content="${image}">
+            <meta name="description" content="${description}">
+            <meta name="twitter:title" content="${title}">
+            <meta name="twitter:description" content="${description}">
+            <meta name="twitter:card" content="summary">
+            <meta name="twitter:image" content="${image}">`;
           return html.replace(/(?<=<head>)/, metaHtml);
         })(html, {
           title: "Wordle Solver",
           description:
             "Get the best guess suggestions for today's Wordle and view future Wordle answers!",
+          image: "/favicon-192x192.png",
         });
       },
     },
